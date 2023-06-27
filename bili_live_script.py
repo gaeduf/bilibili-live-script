80196015d757757580d2d519aad9d2042b52ed17
Shawn Rita <113410236+gaeduf@users.noreply.github.com>
2023-06-27 09:13:04 +0800

    Update bili_live_script.py
    解决重复播放的问题

--- bili_live_script.py
+++ bili_live_script.py
@@ -4,18 +4,16 @@ import io
 import subprocess
 import logging
 
+dir = "/sda/res"  # 你的视频文件所在目录
+rtmp = "?streamname=live_11001546_4006607&key=xxxxxxxx&schedule=rtmp&pflag=1"  # 填你的串流密钥
+fps = "25"  # 默认即可
+start_time = "00:01:30"  # 填你的跳过片头时间
+duration_time = "00:43:30"  # 填你的视频持续时间长
 
-dir = "/sda/res" #你的视频文件所在目录
-rtmp = "?streamname=live_11001546_4006607&key=xxxxxxxx&schedule=rtmp&pflag=1" #填你的串流密钥
-fps = "25" #默认即可
-start_time = "00:01:30" #填你的跳过片头时间
-duration_time = "00:43:30" #填你的视频持续时间长
-
-nameList = []
 
 # 下面方法是查询你的视频目录下的所有视频文件
 def listDir(dirTemp):
-    global nameList
+    nameList = []
     if not os.path.exists(dirTemp):
         print("file or directory doesn't exist")
         return
@@ -32,15 +30,17 @@ def getfilename(file):
     filename = file[tmpint + 1:]
     return filename
 
+
 def main():
     logging.basicConfig(filename='playback.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+    nameList = listDir(dir)
+    nameList.sort()
     while True:
-        List = listDir(dir)
-        List.sort()
-        for file in List:
+        for file in nameList:
             filename = os.path.basename(file)
             logging.info("当前播放的是：" + filename)
-            command = 'ffmpeg -re -ss "'+ start_time +'" -to "'+ duration_time +'" -i "' + file + '" -vcodec copy -acodec copy   -b:a 128k -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
+            command = 'ffmpeg -re -ss "' + start_time + '" -to "' + duration_time + '" -i "' + file + '" -vcodec copy -acodec copy   -b:a 128k -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
             subprocess.run(command, shell=True)
+
 
 main()
