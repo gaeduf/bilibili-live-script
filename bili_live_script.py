eb5ad3cb08c3ea605f707634c4b9f61c710b09c7
Shawn Rita <113410236+gaeduf@users.noreply.github.com>
2023-07-01 15:42:44 +0800

    Update bili_live_script.py
    1）完善跳过片头片尾功能
    2）新增从第几集播放的功能

--- bili_live_script.py
+++ bili_live_script.py
@@ -3,12 +3,13 @@ import os.path
 import io
 import subprocess
 import logging
+import re
 
 dir = "/sda/res"  # 你的视频文件所在目录
-rtmp = "?streamname=live_11001546_4006607&key=xxxxxxxx&schedule=rtmp&pflag=1"  # 填你的串流密钥
+rtmp = "?streamname=live_11001546_4006607&key=xxxxxxxxxxxxxxxxx&schedule=rtmp&pflag=1"  # 填你的串流密钥
 fps = "25"  # 默认即可
-start_time = "00:01:30"  # 填你的跳过片头时间
-duration_time = "00:43:30"  # 填你的视频持续时间长
+beg = 90   # 填你的跳过片头时间 单位：秒
+end = 100  # 这是片尾时间 单位：秒
 
 
 # 下面方法是查询你的视频目录下的所有视频文件
@@ -35,12 +36,32 @@ def main():
     logging.basicConfig(filename='playback.log', level=logging.INFO, format='%(asctime)s - %(message)s')
     nameList = listDir(dir)
     nameList.sort()
+    
     while True:
         for file in nameList:
             filename = os.path.basename(file)
-            logging.info("当前播放的是：" + filename)
-            command = 'ffmpeg -re -ss "' + start_time + '" -to "' + duration_time + '" -i "' + file + '" -vcodec copy -acodec copy   -b:a 128k -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
-            subprocess.run(command, shell=True)
-
+            found_start_file = True
+            start_file = "23.mp4"  # 指定起始文件名 这里可以填你想从第几集播放的上一集名称，比如你想从第二集播放，这里就填第一集的名字， 然后把 found_start_file 的值改成 false。
+            if not found_start_file:
+                if filename == start_file:
+                    found_start_file = True
+                continue
+            
+            # 执行命令并捕获输出
+            cmd = 'nohup ffmpeg -i '+ file + " 2>&1 > tmp.log &"
+            output = subprocess.check_output(cmd, shell=True, text=True)
+            # 使用正则表达式提取 Duration 信息
+            match = re.search(r'Duration: (\d+):(\d+):(\d+\.\d+)', output)
+            if match:
+                hours, minutes, seconds = map(float, match.groups())
+                duration = hours * 3600 + minutes * 60 + seconds
+                # 总时长减去开头和结尾就是截取后的视频时长
+                newtime = duration - beg - end
+                logging.info("当前播放的是：" + filename)
+                command = 'ffmpeg -re -ss "' + str(beg) + '" -to "' + str(newtime) + '" -i "' + file + '" -vcodec copy -acodec copy   -b:a 128k -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
+                subprocess.run(command, shell=True)
+            else:
+                print('无法提取视频时长信息')
+            
 
 main()
