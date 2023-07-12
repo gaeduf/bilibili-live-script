84fcbf18f4901b115fca56f749a5890f109e65ad
Shawn Rita <113410236+gaeduf@users.noreply.github.com>
2023-07-12 11:37:51 +0800

    Update bili_live_script.py
    删除片头片尾跳过功能

--- bili_live_script.py
+++ bili_live_script.py
@@ -8,9 +8,6 @@ import re
 dir = "/sda/res"  # 你的视频文件所在目录
 rtmp = "?streamname=live_11001546_4006607&key=xxxxxxxxxxxxxxxxx&schedule=rtmp&pflag=1"  # 填你的串流密钥
 fps = "25"  # 默认即可
-beg = 90   # 填你的跳过片头时间 单位：秒
-end = 100  # 这是片尾时间 单位：秒
-
 
 # 下面方法是查询你的视频目录下的所有视频文件
 def listDir(dirTemp):
@@ -45,22 +42,9 @@ def main():
                 if filename == start_file:
                     found_start_file = True
                 continue
-            
-            # 执行命令并捕获输出
-            cmd = 'nohup ffmpeg -i '+ file + " 2>&1 > tmp.log &"
-            output = subprocess.check_output(cmd, shell=True, text=True)
-            # 使用正则表达式提取 Duration 信息
-            match = re.search(r'Duration: (\d+):(\d+):(\d+\.\d+)', output)
-            if match:
-                hours, minutes, seconds = map(float, match.groups())
-                duration = hours * 3600 + minutes * 60 + seconds
-                # 总时长减去开头和结尾就是截取后的视频时长
-                newtime = duration - beg - end
-                logging.info("当前播放的是：" + filename)
-                command = 'ffmpeg -re -ss "' + str(beg) + '" -to "' + str(newtime) + '" -i "' + file + '" -vcodec libx264 -acodec aac -ar 44100 -strict -2  -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
-                subprocess.run(command, shell=True)
-            else:
-                print('无法提取视频时长信息')
-            
+            logging.info("当前播放的是：" + filename)
+            command = 'ffmpeg -re -i "' + file + '" -vcodec libx264 -acodec aac -ar 44100 -strict -2  -r ' + fps + ' -f flv "rtmp://live-push.bilivideo.com/live-bvc/' + rtmp + '"'
+            subprocess.run(command, shell=True)
+          
 
 main()
